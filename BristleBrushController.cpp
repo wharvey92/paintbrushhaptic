95390f9ad89c192cae04224370ee3692533366e5
Diego Canales <diegoc1@stanford.edu>
2014-05-26 21:41:51 -0700

    brist files

---
+++ BristleBrushController.cpp
@@ -0,0 +1,9 @@
+//
+//  BristleBrushController.cpp
+//  GEL
+//
+//  Created by Diego Canales on 5/26/14.
+//
+//
+
+#include "BristleBrushController.h"
---
+++ BristleBrushController.h
@@ -0,0 +1,14 @@
+//
+//  BristleBrushController.h
+//  GEL
+//
+//  Created by Diego Canales on 5/26/14.
+//
+//
+
+#ifndef __GEL__BristleBrushController__
+#define __GEL__BristleBrushController__
+
+#include <iostream>
+
+#endif /* defined(__GEL__BristleBrushController__) */
