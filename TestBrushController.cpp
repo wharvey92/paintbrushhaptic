59ae190defc57d6475dc560f2eec6d56776a713c
Diego Canales <diegoc1@stanford.edu>
2014-05-28 19:17:49 -0700

    got textures working

---
+++ TestBrushController.cpp
@@ -0,0 +1,9 @@
+//
+//  TestBrushController.cpp
+//  GEL
+//
+//  Created by Diego Canales on 5/28/14.
+//
+//
+
+#include "TestBrushController.h"
---
+++ TestBrushController.h
@@ -0,0 +1,14 @@
+//
+//  TestBrushController.h
+//  GEL
+//
+//  Created by Diego Canales on 5/28/14.
+//
+//
+
+#ifndef __GEL__TestBrushController__
+#define __GEL__TestBrushController__
+
+#include <iostream>
+
+#endif /* defined(__GEL__TestBrushController__) */
